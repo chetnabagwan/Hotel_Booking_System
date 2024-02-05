d8f775c0fd01aac53562e6eeb67b3b154460b168
chetnabagwan <bagwanchetna66@gmail.com>
2024-02-05 13:47:48 +0530

    Refactored code

--- src/views/auth_views.py
+++ src/views/auth_views.py
@@ -5,6 +5,7 @@ from jose import JWTError, jwt
 import logging
 from fastapi import APIRouter, Depends,HTTPException
 from datetime import datetime,timedelta
+from blocklist import BLOCKLIST
 from controllers.auth_controller import Authentication
 from utils.config_class import Config
 
@@ -22,7 +23,7 @@ def create_access_token(user_id:str,role:str,expires_delta:timedelta):
     return jwt.encode(payload,Config.SECRET_KEY,algorithm=Config.ALGORITHM)
 
 
-@auth_router.post("/login",status_code=status.HTTP_200_OK) #working
+@auth_router.post("/login",status_code=status.HTTP_200_OK)
 def login_and_gen_token(user_data: OAuth2PasswordRequestForm=Depends()):
     logger.info(f'User with username : {user_data.username} tries to login into the application')
 
@@ -33,12 +34,10 @@ def login_and_gen_token(user_data: OAuth2PasswordRequestForm=Depends()):
             token = create_access_token(str(data[0]),data[1],timedelta(minutes=15))
             return {'access_token':token,'token_type':'Bearer'}
     except :
-        # raise e
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=Config.INVALID_CREDENTIALS)
 
 
 async def get_current_user(token:Annotated[str,Depends(oauth2_scheme)]):
-    # print(token)
     try:
         payload = jwt.decode(token,Config.SECRET_KEY,algorithms=[Config.ALGORITHM])
         user_id = payload.get('sub')
@@ -49,4 +48,11 @@ async def get_current_user(token:Annotated[str,Depends(oauth2_scheme)]):
     except JWTError as e:
         print(e)
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=Config.UNAUTHENTICATED_USER)
+
+@auth_router.post("/logout",status_code=status.HTTP_200_OK)
+async def logout(token:Annotated[str,Depends(oauth2_scheme)],user:Annotated[dict,Depends(get_current_user)]):
+        if user['user_id'] is None or user['role'] is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail=Config.UNAUTHENTICATED_USER)  
+        BLOCKLIST.add(token)
+
 
